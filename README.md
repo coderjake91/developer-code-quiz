0155e0fad14fc520e321c14fd859348f8e993e8b
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-14 17:02:34 -0600

    set up initial file structure, and webpage layout for coding quiz using bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # developer-code-quiz
 Coding quiz created for aspiring developers to test their coding knowledge
+
+https://www.fullstack.cafe/interview-questions/javascript
---
+++ assets/js/code-quiz.js
@@ -0,0 +1,9 @@
+/*-------------------------------*/
+//Code-quiz pseudocode
+/*
+Note: quiz is ten questions long, and user is give 1 min to answer
+-> user clicks start
+-> then, first question is presented and time starts concurrently (on click event here)
+-> user selects answer choice (either incorrect or correct selection)
+*/
+/*-------------------------------*/
